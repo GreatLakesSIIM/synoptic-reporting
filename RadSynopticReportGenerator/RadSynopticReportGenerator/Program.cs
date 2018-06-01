c33df2f9474284136fcf058457494b16b40947ca
Peter Dobbs <dobbsp8@gmail.com>
2018-06-01 00:45:24 -0400

    restclient implementation
    simpler code for modularity and expanding

--- RadSynopticReportGenerator/RadSynopticReportGenerator/Program.cs
+++ RadSynopticReportGenerator/RadSynopticReportGenerator/Program.cs
@@ -1,55 +1,40 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 
 namespace RadSynopticReportGenerator {
   class Program {
 
-    class RadAppDelegatingHandler : DelegatingHandler {
-      private string apiKey => Environment.GetEnvironmentVariable("SiimApiKey");
-      private DateTime beginningOfEpoch => new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
-
-      protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
-        var response = new HttpResponseMessage();
-
-        request.Headers.Authorization = new AuthenticationHeaderValue("apikey", apiKey);
-
-        response = await base.SendAsync(request, cancellationToken);
-        return response;
-      }
-    }
-
-    public class Patient {
-      public string PatientID { get; set; }
-    }
-
     private static string baseFhir => "http://hackathon.siim.org/fhir/";
     private static string baseDicomWeb => "http://hackathon.siim.org/dicomweb/";
 
-    private static string exampleExtension => "Patient?family=SIIM";
+    static void Main(string[] args) {
+      //var response = runApiRequest(Method.GET, baseFhir, "Patient?name=siimjoe");
+      //var results = JsonConvert.DeserializeObject<dynamic>(response.Content);
+      //var entry = results.entry;
 
-    static async Task RunAsync() {
-      var delegatingHandler = new RadAppDelegatingHandler();
-      var client = HttpClientFactory.Create(delegatingHandler);
-      var patient = new Patient { PatientID = "1234" };
-
-      var postString = baseFhir + ""; //could also use baseDicomWeb
-      var response = await client.PostAsXmlAsync(postString, patient);
-
-      if (response.IsSuccessStatusCode) {
-        var responseString = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(responseString);
-        Console.WriteLine("HTTP Status: {0}, Reason {1}. Press ENTER to exit", response.StatusCode, response.ReasonPhrase);
-      } else {
-        Console.WriteLine("Failed to call the API. HTTP Status: {0}, Reason {1}", response.StatusCode, response.ReasonPhrase);
-      }
-      Console.ReadLine();
+      getDiagnosticReportsForSubjectByProcedureCode("siimjoe", "24627-2");
     }
 
-    static void Main(string[] args) {
-      RunAsync().Wait();
+    //information for the comoparison studies
+    static void getDiagnosticReportsForSubjectByProcedureCode(string subject, string procedureCode) {
+      var response = runApiRequest(Method.GET, baseFhir, $"DiagnosticReport?subject={subject}&code={procedureCode}");
+      var results = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+      var mostRecentDate = results.entry[0].resource.effectiveDateTime.Value;
+      var mostRecentConclusion = results.entry[0].resource.conclusion.Value;
+
+      System.IO.File.WriteAllText(@"C:\Users\Peter\Documents\GitHub\output.txt", response.Content.ToString());
+
+      //var diagnosticReports = JsonDomFhirNavigator.Create(response.Content);
+    }
+
+    static IRestResponse runApiRequest(RestSharp.Method method, string endpoint, string query) {
+      var request = new RestRequest(method);
+      var client = new RestClient(endpoint + query);
+      request.AddHeader("apikey", Environment.GetEnvironmentVariable("SiimApiKey"));
+      var result = client.Execute(request);
+      return result;
     }
   }
 }
