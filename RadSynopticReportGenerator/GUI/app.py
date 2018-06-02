1def46265cf55e4e0ae0f31aecb04ba592613fe9
znordgren <zachary.nordgren@marquette.edu>
2018-06-02 05:23:33 -0400

    got json to export

--- RadSynopticReportGenerator/GUI/app.py
+++ RadSynopticReportGenerator/GUI/app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -48,7 +48,6 @@ class Application(tk.Frame):
     def getForm(self):
         self.formInfo['req'] = requests.get('http://radreport.org/html/htmldownload.php?id={}'.format(self.formInfo['formID'].get()))
         self.formInfo['bsForm'] = BeautifulSoup(self.formInfo['req'].text,'html.parser')
-        print(self.formInfo['bsForm'].prettify())
         self.buildForm()
 
     def buildForm(self):
@@ -59,7 +58,8 @@ class Application(tk.Frame):
         self.formFrame.title.grid(row=2,column=1,columnspan=2)
         
         coding = self.formInfo['bsForm']('script')[0].text
-        self.formInfo['coding'] = {x.replace('<!--','').replace('-->','') for x in coding}
+        self.formInfo['coding'] = coding.replace('<!--','').replace('-->','')
+        self.formInfo['fields'] = {}
 
         iRow = 3
         for section in self.formInfo['bsForm']('section'):
@@ -67,34 +67,32 @@ class Application(tk.Frame):
                 self.addTitleRow(self.formFrame.templateFrame,section.header.string,iRow)
                 iRow += 1
                 for row in section.find_all('p'):
-                    print(row.input['id'])
                     self.addTextFieldRow(self.formFrame.templateFrame,row.input['id'],row.label.string,row.input['value'],iRow)
                     iRow += 1
         
         self.formFrame.postButton = tk.Button(master=self.formFrame.templateFrame,text='Post',command=self.jsonDump,padx=10)
         self.formFrame.postButton.grid(row=iRow,column=1)
 
-    def addFindingsSection(self, findings, iRow):
-        #this section will do the finding but I'm going to just use the parent function for now until I get it to work
-        print("Build finding block")
-
     def addTitleRow(self, parent, string, iRow):
         print('titles skipped for now')
 
     def addTextFieldRow(self, parent, id, name, defaultValue, iRow):
         #setattr(self.formFrame,id,tk.Entry())
-        self.formInfo[id] = {'name':name,'value':tk.StringVar(value=defaultValue)}
-        print(self.formInfo[id])
+        self.formInfo['fields'][id] = {'name':name,'value':tk.StringVar(value=defaultValue)}
         setattr(self.formFrame,id+'Label',tk.Label(master=parent, text=name))
         getattr(self.formFrame,id+'Label').grid(row=iRow,column=0,columnspan=2)
-        setattr(self.formFrame,id+'entry',tk.Entry(master=parent,width=50, textvariable=self.formInfo[id]['value']))
+        setattr(self.formFrame,id+'entry',tk.Entry(master=parent,width=50, textvariable=self.formInfo['fields'][id]['value']))
         getattr(self.formFrame,id+'entry').grid(row=iRow,column=2,columnspan=3)
 
     def addDropDownRow(self, parent, id, name, iRow):
         print('drop down skipped for now')
 
     def jsonDump(self):
-        print('this will dump the json data')
+        self.patientInfo['dump'] = {'coding':self.formInfo['coding']}
+        self.patientInfo['dump']['form'] = {}
+        for field, values in self.formInfo['fields'].items():
+            self.patientInfo['dump']['form'][field] = {'name':values['name'],'value':values['value'].get()}
+        print(json.dumps(self.patientInfo['dump']))
 
 app = Application()
 app.master.title('Radiology Synoptic Report Generator')
