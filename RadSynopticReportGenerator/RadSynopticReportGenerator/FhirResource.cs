ab85b666b2d5bf19af13a12529afafc1f53c6e87
Peter Dobbs <dobbsp8@gmail.com>
2018-06-23 17:52:26 +0300

    updating organization of support functions

---
+++ RadSynopticReportGenerator/RadSynopticReportGenerator/FhirResource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hl7.Fhir.Model;
+
+
+namespace RadSynopticReportGenerator {
+  class FhirResource {
+
+    public static Patient DefaultPatient => new Patient() {
+      Identifier = new List<Identifier> { new Identifier("MCW", "yuengling") },
+      Active = true,
+      Name = new List<HumanName> { new HumanName() { GivenElement = new List<FhirString> { new FhirString("example") } } },
+      Telecom = new List<ContactPoint> { new ContactPoint() },
+      Gender = AdministrativeGender.Unknown,
+      BirthDateElement = new Date(1996),
+      Address = new List<Address> { new Address() { City = "Milwaukee" } },
+      MaritalStatus = new CodeableConcept("http://hl7.org/fhir/v3/MaritalStatus", "S", "Never Married"),
+      Photo = new List<Attachment> { new Attachment() },
+      Contact = new List<Patient.ContactComponent> { new Patient.ContactComponent {
+            Relationship = new List<CodeableConcept> { new CodeableConcept("http://hl7.org/fhir/v2/0131", "C", "Emergency Contact") },
+            Name = new HumanName() { GivenElement = new List<FhirString> { new FhirString("contact") } },
+            Telecom = new List<ContactPoint> { new ContactPoint() },
+            Address = new Address() { City = "Milwaukee" } ,
+            Gender = AdministrativeGender.Unknown,
+            Organization = new ResourceReference("ref","disp"),
+            Period = new Period(new FhirDateTime(1996),new FhirDateTime(2019))} },
+      Communication = new List<Patient.CommunicationComponent> { new Patient.CommunicationComponent() {
+          Language = new CodeableConcept("http://hl7.org/fhir/ValueSet/languages", "en-US", "English (United States)"),
+          Preferred = true } },
+      GeneralPractitioner = new List<ResourceReference>(),
+      //ManagingOrganization = new ResourceReference("MU-MCW", "TeamGreatLakes"),
+      //Link = new List<Patient.LinkComponent> { new Patient.LinkComponent { Other = new ResourceReference("MU-MCW", "TeamGreatLakes"), Type = Patient.LinkType.Refer } }
+    };
+
+  }
+}
--- RadSynopticReportGenerator/RadSynopticReportGenerator/RestfulProcedures.cs
+++ RadSynopticReportGenerator/RadSynopticReportGenerator/RestfulProcedures.cs
@@ -26,7 +26,7 @@ namespace RadSynopticReportGenerator {
         PreferredFormat = ResourceFormat.Json
       };
       client.OnBeforeRequest += (object sender, BeforeRequestEventArgs e) => {
-        e.RawRequest.Headers.Add("apikey", Environment.GetEnvironmentVariable("SiimApiKey"));
+        e.RawRequest.Headers.Add("apikey", Environment.GetEnvironmentVariable("SiimApiKey")); //requires environment variable to match
       };
       return client;
     }
@@ -79,34 +79,12 @@ namespace RadSynopticReportGenerator {
     }
 
     public static Result<bool, string> PostDiagnosticReport() {
-      var patient = new Patient() {
-        Identifier = new List<Identifier> { new Identifier("MCW", "yuengling") },
-        Active = true,
-        Name = new List<HumanName> { new HumanName() { GivenElement = new List<FhirString> { new FhirString("example") } } },
-        Telecom = new List<ContactPoint> { new ContactPoint() },
-        Gender = AdministrativeGender.Unknown,
-        BirthDateElement = new Date(1996),
-        Address = new List<Address> { new Address() { City = "Milwaukee" } },
-        MaritalStatus = new CodeableConcept("http://hl7.org/fhir/v3/MaritalStatus", "S", "Never Married"),
-        Photo = new List<Attachment> { new Attachment() },
-        Contact = new List<Patient.ContactComponent> { new Patient.ContactComponent {
-            Relationship = new List<CodeableConcept> { new CodeableConcept("http://hl7.org/fhir/v2/0131", "C", "Emergency Contact") },
-            Name = new HumanName() { GivenElement = new List<FhirString> { new FhirString("contact") } },
-            Telecom = new List<ContactPoint> { new ContactPoint() },
-            Address = new Address() { City = "Milwaukee" } ,
-            Gender = AdministrativeGender.Unknown,
-            Organization = new ResourceReference("ref","disp"),
-            Period = new Period(new FhirDateTime(1996),new FhirDateTime(2019))} },
-        Communication = new List<Patient.CommunicationComponent> { new Patient.CommunicationComponent() {
-          Language = new CodeableConcept("http://hl7.org/fhir/ValueSet/languages", "en-US", "English (United States)"),
-          Preferred = true } },
-        GeneralPractitioner = new List<ResourceReference>(),
-        //ManagingOrganization = new ResourceReference("MU-MCW", "TeamGreatLakes"),
-        //Link = new List<Patient.LinkComponent> { new Patient.LinkComponent { Other = new ResourceReference("MU-MCW", "TeamGreatLakes"), Type = Patient.LinkType.Refer } }
-      };
+      var patient = FhirResource.DefaultPatient;
       OpenFhirClient(baseFhir).Create(patient);
+
       //create diagnostic report with random identifier, assigning authority = MCW
       var study = GetImagingStudy();
+
       var dx = new DiagnosticReport {
         Identifier = patient.Identifier,
         Id = "siimdxrpt77",
@@ -114,6 +92,7 @@ namespace RadSynopticReportGenerator {
         Code = new CodeableConcept("MCW", "birdhouse")
       };
       OpenFhirClient(baseFhir).Create(dx);
+
       return Ok<bool, string>(true);
     }
   }
